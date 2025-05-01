9c2e33934f42083af0747e12f22ea98b440c9c88
DaniDataCodes <daniel.tepstein@gmail.com>
2025-05-01 19:57:57 -0400

    pysthon stuff 1

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
 print("hello world!")
+
+name = "Dani "
+instructor = "Ben "
+
+print("hello " ,name)
+print("I am " ,instructor)
---
+++ square.py
@@ -0,0 +1,5 @@
+#This program asks the user for a number and provides the square
+
+x = input("choose a number: ")
+square = int(x) * int(x)
+print(f"THe Square of {x} is {square}")
