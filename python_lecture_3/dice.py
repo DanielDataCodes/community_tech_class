5ee7b8b6b5b327bd18dc8e95d673897a8432a787
DaniDataCodes <daniel.tepstein@gmail.com>
2025-05-13 15:36:38 -0400

    added robust dice game

--- python_lecture_3/dice.py
+++ python_lecture_3/dice.py
@@ -1,16 +1,31 @@
 ##my_list = [1, 2, 3, 4, 5]
 ##for item in my_list:
 ##    print(item)
-
-type_of_die = int(input("choose a die (6,12,20) "))
+import random
 
 while True:
-    if type_of_die not in [6, 12, 20]:
-        print("Must be 6, 12 or 20")
-match type_of_die:
-    case 6:
-        print("you chose 6")
-    case 12:
-        print("you chose 12")
-    case 20:
-        print("you chose 20")
+    type_of_die = int(input("choose a die (6,12,20) "))
+
+    while type_of_die not in [6, 12, 20]:
+            print("Must be 6, 12 or 20")
+            type_of_die = int(input("choose a die (6, 12, 20) "))
+    while True:
+        match type_of_die:
+            case 6:
+                roll = random.randint(1,6)
+                print(f"you chose 6 and rolled {roll}")
+            case 12:
+                roll = random.randint(1,12)
+                print(f"you chose 12 and rolled {roll}")
+            case 20:
+                roll = random.randint(1,20)
+                print(f"you chose 20 and rolled {roll}")
+        
+        roll_again = input("roll again with the same die y/n? ")
+        if roll_again != "y":
+            break
+    change_die = input("Change die y/n? " )
+    if change_die != "y":
+        break
+
+print("thanks for playing")
